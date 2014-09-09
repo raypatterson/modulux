6fc3cd6dea68e0a78b43c613594d0a5d5f16c283
Ray Patterson <ray.patterson@SFMB55FD57L.local>
2014-09-08 22:40:55 -0700

    Fixed image copy task.

--- grunt/tasks/compile.js
+++ grunt/tasks/compile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('compile', [
     'clean',
+    'copy:dist',
     'swig:dist',
     'htmlmin',
     'webpackconfig',
--- grunt/tasks/options/copy.js
+++ grunt/tasks/options/copy.js
@@ -6,10 +6,13 @@ module.exports = {
       src: ['<%= app_files.copy.src %>'],
       dest: '<%= build_dir %>'
     }]
+  },
+  dist: {
+    files: [{
+      expand: true,
+      cwd: '<%= app_files.copy.cwd %>',
+      src: ['<%= app_files.copy.src %>'],
+      dest: '<%= compile_dir %>'
+    }]
   }
 }
-
-// [
-//   '<%= build_dir %>',
-//   '<%= compile_dir %>'
-// ];
