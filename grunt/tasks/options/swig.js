45f6b107e29e2c706481f3899ed624663dc76017
Ray Patterson <ray.patterson@SFMB55FD57L.local>
2014-09-27 17:11:31 -0700

    Moved methods from Swig task config  into Swig task.

--- grunt/tasks/options/swig.js
+++ grunt/tasks/options/swig.js
@@ -3,57 +3,15 @@
  * https://github.com/RayPatterson/grunt-swig2
  */
 
-var grunt = require('grunt');
-var fs = require('fs-extra');
-var _ = require('lodash');
-
-var resources = {};
-
 module.exports = {
   options: {
-    basepath: '<%= app_files.swig.basepath %>',
-    pagepath: '<%= app_files.swig.pages.cwd %>',
+    config: '<%= app_files.swig %>',
+    webpack: 'app_files.webpackrequire',
     swigOptions: {
       autoescape: true,
       cache: false,
       locals: {
-        rootDir: '<%= root_dir %>',
-        getPartialPath: function(slug, item) {
-          var cfg = grunt.config.get('app_files.swig.partials');
-          var src = cfg.src + slug + cfg.filepath;
-          return src;
-        },
-        setDefaultsData: function(slug, item) {
-          var cfg = require('grunt').config.get('app_files.swig.partials');
-          var src = cfg.src + slug + cfg.datapath;
-          item.defaults = grunt.file.exists(src) ? grunt.file.readJSON(src) : {};
-
-          // console.log('item', item);
-          return item;
-        },
-        addResources: function(pathname, item, page) {
-
-          // TODO: Here is where the Webpack resources need to be required, REMEMBER WHERE YOU WERE!
-          // NOTE: page comes from the Swig Grunt task you modified
-
-          var cfg = grunt.config('app_files.webpackrequire');
-
-          var resources = cfg.resources || {};
-
-          if (!resources[page]) {
-            resources[page] = [];
-          }
-
-          var requires = grunt.file.expand({
-            cwd: cfg.partials.cwd + pathname
-          }, cfg.partials.match);
-
-          _.each(requires, function(require) {
-            resources[page].push(pathname + '/' + require);
-          });
-
-          grunt.config('app_files.webpackrequire.resources', resources);
-        }
+        rootDir: '<%= root_dir %>'
       }
     },
     data: {},
--- grunt/tasks/options/webpackconfig.js
+++ grunt/tasks/options/webpackconfig.js
@@ -1,8 +1,3 @@
 module.exports = {
-  dev: {
-    watch: '<%= app_files.webpackconfig.watch %>',
-    match: '<%= app_files.webpackconfig.match %>',
-    cwd: '<%= app_files.webpackconfig.cwd %>',
-    ext: '<%= app_files.webpackconfig.ext %>'
-  }
+  dev: '<%= app_files.webpackconfig %>'
 };
