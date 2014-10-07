5ff19c380f057a351f37055d721e2c17ac1ef057
Ray Patterson <ray.patterson@SFMB55FD57L.local>
2014-10-06 22:29:07 -0700

    Fixed Dropdown.

--- src/project/swig/partials/modules/header/index.js
+++ src/project/swig/partials/modules/header/index.js
@@ -1,13 +1,20 @@
 require('utils/hello.js')('Init Header', '.header');
 
+require('bootstrap-sass-official/assets/javascripts/bootstrap/dropdown.js');
+
 $(function() {
+
   var $el;
-  console.log('location.pathname', location.pathname);
-  $('.header-menu-list-item ').each(function(idx, el) {
+
+  $('.header-menu li').each(function(idx, el) {
     console.log('el', el);
     $el = $(el);
     if (location.pathname === $el.children('a').attr('href')) {
       $el.addClass('active');
     }
   });
+
+  $el = $('.header-menu .dropdown-toggle')
+  $el.dropdown();
+
 });
