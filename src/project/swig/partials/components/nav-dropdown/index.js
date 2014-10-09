ad3bac8503c3eabeed07973f698e72ff090fa2ab
Ray Patterson <ray.patterson@SFMB55FD57L.local>
2014-10-09 11:59:42 -0700

    Dropdown enabled.

--- src/project/swig/partials/components/nav-dropdown/index.js
+++ src/project/swig/partials/components/nav-dropdown/index.js
@@ -2,13 +2,13 @@ require('bootstrap-sass-official/assets/javascripts/bootstrap/dropdown.js');
 
 require('utils/hello.js')('Init Dropdown', '.dropdown-toggle');
 
-// $(function() {
+$(function() {
 
-//   var $el = $('.dropdown-toggle');
+  var $el = $('.dropdown-toggle');
 
-//   $el.click(function() {
-//     window.location.href = $(this).attr('href');;
-//     return false;
-//   }).dropdown();
+  $el.click(function() {
+    window.location.href = $(this).attr('href');;
+    return false;
+  })
 
-// });
+});
--- src/project/swig/partials/modules/header/index.js
+++ src/project/swig/partials/modules/header/index.js
@@ -1,7 +1,5 @@
 require('utils/hello.js')('Init Header', '.header');
 
-require('bootstrap-sass-official/assets/javascripts/bootstrap/dropdown.js');
-
 $(function() {
 
   var $el;
@@ -11,13 +9,6 @@ $(function() {
     if (location.pathname === $el.children('a').attr('href')) {
       $el.addClass('active');
     }
-  });
-
-  $el = $('.dropdown-toggle');
-
-  $el.click(function() {
-    window.location.href = $(this).attr('href');;
-    return false;
   });
 
 });
