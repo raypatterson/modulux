e96495d96f181b9ccbd21d6e97bda089396b9cf8
Ray Patterson <ray.patterson@SFMB55FD57L.local>
2014-10-14 13:29:06 -0700

    Added a tout.

--- src/_entry/page.js
+++ src/_entry/page.js
@@ -1,5 +1,9 @@
 // Bootstrap
 require('bootstrap-sass-official/assets/stylesheets/_bootstrap.scss');
+require('bootstrap-material-design/sass/ripples.scss');
+require('bootstrap-material-design/sass/material-wfont.scss');
+require('bootstrap-material-design/scripts/ripples.js');
+require('bootstrap-material-design/scripts/material.js');
 
 // Custom
 require('common/styles/variables.scss');
